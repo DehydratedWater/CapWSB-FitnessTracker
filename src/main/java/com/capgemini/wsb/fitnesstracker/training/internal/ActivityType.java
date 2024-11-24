06af69327d40f62306526949f846c888a5989201
MichalWieczorek23 <163677658+MichalWieczorek23@users.noreply.github.com>
2024-11-24 13:38:13 +0100

    Solution has been supplemented with javadoc elements from the TODO section in ActivityType

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/ActivityType.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/ActivityType.java
@@ -1,20 +1,57 @@
 package com.capgemini.wsb.fitnesstracker.training.internal;
 
-// TODO : JavaDoc
+
+/**
+ * Enum representing the different types of activiies: RUNNING, CYCLING, WALKING, SWIMMING,
+ * TENNIS. These activities can be performed by user during training session.
+ *
+ */
 public enum ActivityType {
 
+
+    /**
+     * Represents a running activity
+     */
     RUNNING("Running"),
+
+    /**
+     * Represents a cycling activity
+     */
     CYCLING("Cycling"),
+
+    /**
+     * Represents a walking activity
+     */
     WALKING("Walking"),
+
+    /**
+     * Represents a swimming activity
+     */
     SWIMMING("Swimming"),
+
+    /**
+     * Represents a tennis activity
+     */
     TENNIS("Tenis");
 
     private final String displayName;
 
+
+    /**
+     * Constructs a new ActivityType with a specified displayName
+     *
+     * @param displayName readable name for the activity type
+     */
     ActivityType(String displayName) {
         this.displayName = displayName;
     }
 
+
+    /**
+     * Returns the displayName of the activity type.
+     *
+     * @return displayName of the activity type
+     */
     public String getDisplayName() {
         return displayName;
     }
